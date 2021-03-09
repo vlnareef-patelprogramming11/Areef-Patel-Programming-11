12d8d11c2145c3aa895a7f39f854c4dcd63c9d03
vlnareef-patelprogramming11 <80241458+vlnareef-patelprogramming11@users.noreply.github.com>
2021-03-08 17:43:20 -0800

    Expression Areef Patel handin

---
+++ Expressions Assignment/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    //Expression 1
+        System.out.println((9 - 4)+ 10/2 - (4 - 8)/3);
+//11.3
+    //Expression 2
+        System.out.println(23%4 + (18 - 3 / 2));
+//19.5
+    //Expression 3
+        System.out.println(((8 - 5) + Math.pow(3,4)/5));
+//19.2
+    //Expression 4
+        System.out.println((9 % 4) - 8 + Math.pow(2,3) / 8 + (Math.sqrt(81) / 3));
+//11
+    //Expression 5
+        System.out.println(((14 / 2 + 3) * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
+//-116
+    }
+}
+
