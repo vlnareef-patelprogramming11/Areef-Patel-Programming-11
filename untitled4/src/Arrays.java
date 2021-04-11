be0514276b0249092fc02d2030dbde095ad97059
vlnareef-patelprogramming11 <80241458+vlnareef-patelprogramming11@users.noreply.github.com>
2021-04-11 16:19:31 -0700

    ARrays
    arrays

---
+++ untitled4/src/Arrays.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+
+public class Arrays {
+
+
+
+    public void run() {
+
+        int[] arr = {2, 10, 12, 13, 10, 142};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+        public static void end()
+        {
+
+            int[] arr = new int[5];
+            for(int i=0;i < arr.length; i++)
+            {
+        arr[i] = i+1;
+        }
+        }
+
+
+
+
+    }
+
+
+
+
+
